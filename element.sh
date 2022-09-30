406a4df1200efe8974e9b64d5a2a6cfd2aa8b4b0
Sarang Wadode <sarangwadode@gmail.com>
2022-09-30 23:43:07 +0530

    fix: code optimized

--- element.sh
+++ element.sh
@@ -7,23 +7,16 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
+fi
+
+#if argument is atomic number
+if [[ $1 =~ ^[1-9]+$ ]]
+then
+  element=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
+  echo $element
+ 
 else
-  #get element using argument
-  element_by_an=$($PSQL "select * from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
-  # element_by_symbol=$($PSQL "select * from elements join properties using(atomic_number) join types using(type_id) where symbol = '$1'")
-  # element_by_name=$($PSQL "select * from elements join properties using(atomic_number) join types using(type_id) where name = '$1'")
-
-  #if argument is atomic number
-  if [[ $element_by_an ]]
-  then
-    mp=$($PSQL "select melting_point_celsius from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
-    bp=$($PSQL "select boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
-    name=$($PSQL "select name from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
-    symbol=$($PSQL "select symbol from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
-    type=$($PSQL "select type from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
-    mass=$($PSQL "select atomic_mass from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
-
-    echo "The element with the atomic number $1 is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
-  fi
+  element=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where name = '$1' or symbol = '$1'")
+  echo $element
 fi
 
